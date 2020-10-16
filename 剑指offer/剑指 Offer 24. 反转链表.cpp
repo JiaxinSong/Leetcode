784f8ad0bccb55c20c55409ae122ecec9a1342f4
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2020-10-16 01:05:18 -0400

    Create 剑指 Offer 24. 反转链表.cpp

---
+++ 剑指offer/剑指 Offer 24. 反转链表.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = NULL, *pre = head;
+        while (pre != NULL) {
+            ListNode* t = pre->next;
+            pre->next = cur;
+            cur = pre;
+            pre = t;
+        }
+        return cur;
+    }
+};
+
+//递归
+//一直返回尾节点
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* ret = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return ret;
+    }
+};
+
+
