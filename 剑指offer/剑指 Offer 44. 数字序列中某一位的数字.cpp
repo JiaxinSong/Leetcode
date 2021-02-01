84fb014c53dbb6431a612d928b9bbe682155cfc2
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-01 18:36:31 -0500

    Update 剑指 Offer 44. 数字序列中某一位的数字.cpp

--- 剑指offer/剑指 Offer 44. 数字序列中某一位的数字.cpp
+++ 剑指offer/剑指 Offer 44. 数字序列中某一位的数字.cpp
@@ -5,7 +5,10 @@ class Solution {
 public:
     int findNthDigit(int n) {
         int i = 1;
-        while(n > 0.9*pow(10,i)*i) n -= 0.9*pow(10,i)*i,i++;            //大于则前进 
+        while(n>0.9*pow(10,i)*i) {
+           n-=0.9*pow(10,i)*i;
+           i++;
+       }           //大于则前进 
         string res = to_string(pow(10, i-1) + (n-1)/i);                 //求出n所在的数字，并转化位字符串
         return res[(n-1)%i] - '0';                                      //求出n在字符串第几位，输出。
     }
