f6c03dbc68939ea469c4f440bd39e2027eaf899a
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-03 22:20:33 -0500

    Update 剑指 Offer 58 - I. 翻转单词顺序.cpp

--- 剑指offer/剑指 Offer 58 - I. 翻转单词顺序.cpp
+++ 剑指offer/剑指 Offer 58 - I. 翻转单词顺序.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int left=s.size()-1;
-        int right=s.size()-1;
-
-        string ret="";
-        while(left>=0){
-            if(s[left]==' '){
-                left--;
-                right--;
-                continue;
-            }
-            while(left>=0&& s[left]!=' '){
-                left--;
-            }
-            if(left<right||left<0){
-                ret.append(s.substr(left+1,right-left));
-                ret.append(" ");
-                right=left;
-            }
-
+    string reverseWords(string s)
+{
+    int left = (int)s.size() - 1;
+    int right = (int)s.size() - 1;
+    string ret = "";
+    
+    while(left >= 0)
+    {
+        while(left>=0&&s[left] == ' ')
+        {
+            left--;
+            right--;
         }
-        ret=ret.substr(0,ret.size()-1);
-        return ret;
+        if(left<0) break;
+        
+        while(left >=0 && s[left] != ' ')
+        {
+            left--;
+        }
+        if(left < right || left < 0)
+        {
+            ret += s.substr(left + 1, right - left) + ' ';
+            right = left;
+            continue;
+        }
     }
+    ret = ret.substr(0,ret.size() - 1);//最后删除末尾空格
+    return  ret;
+}
+
 };
