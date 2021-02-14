1a9be67549820b38789458aa2008456b4a9f9b95
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-13 21:24:34 -0500

    Create 889. 根据前序和后序遍历构造二叉树.cpp

---
+++ 经典算法/二叉树/构建二叉树/889. 根据前序和后序遍历构造二叉树.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* constructFromPrePost2(vector<int>& pre, int prel, int prer, vector<int>& post, int postl, int postr) {
+        if(prel > prer)
+            return NULL;
+        else if(prel == prer)
+            return new TreeNode(pre[prel]);
+        TreeNode* node = new TreeNode(pre[prel]);
+        for(int i = postl; i<=postr; i++){
+            if(post[i] == pre[prel+1]) {
+                node->left = constructFromPrePost2(pre, prel+1, prel+1+(i-postl), post, postl, i);
+                node->right = constructFromPrePost2(pre, prel+2+(i-postl), prer, post, i+1, postr);
+            }
+        }
+        return node;
+    }
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        return constructFromPrePost2(pre, 0, pre.size()-1, post, 0, post.size()-1);
+    }
+};
