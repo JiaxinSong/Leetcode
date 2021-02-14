832c7b96ab79596b8e5e33656a140cfeba2cb748
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-13 21:34:22 -0500

    Update 889. 根据前序和后序遍历构造二叉树.cpp

--- 经典算法/二叉树/构建二叉树/889. 根据前序和后序遍历构造二叉树.cpp
+++ 经典算法/二叉树/构建二叉树/889. 根据前序和后序遍历构造二叉树.cpp
@@ -9,7 +9,7 @@ public:
         for(int i = postl; i<=postr; i++){
             if(post[i] == pre[prel+1]) {
                 node->left = constructFromPrePost2(pre, prel+1, prel+1+(i-postl), post, postl, i);
-                node->right = constructFromPrePost2(pre, prel+2+(i-postl), prer, post, i+1, postr);
+                node->right = constructFromPrePost2(pre, prel+2+(i-postl), prer, post, i+1, postr-1);
             }
         }
         return node;
