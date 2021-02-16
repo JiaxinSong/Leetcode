96cca486572306633ab5e189a94a9f55d5430dad
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-15 19:02:45 -0500

    Update 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.cpp

--- 剑指offer/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.cpp
+++ 剑指offer/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.cpp
@@ -1,3 +1,24 @@
+
+class Solution {
+public:
+    vector<int> exchange(vector<int>& nums) {
+        int left =0,right=nums.size()-1;
+        while(left<right){
+            if((nums[left]&1)!=0){
+                left++;
+                continue;
+            }
+            if((nums[right]&1)!=1){
+                right--;
+                continue;
+            }
+            swap(nums[left++],nums[right--]);
+          }
+          return nums;
+    }
+};
+
+
 //快慢指针 fast找奇数 slow是偶数
 
 class Solution {
