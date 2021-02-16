e4ee8cd245511a681a098cee242d28e2bc2959fa
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-02-16 18:32:04 -0500

    Create 剑指 Offer 26. 树的子结构.cpp

---
+++ 剑指offer/剑指 Offer 26. 树的子结构.cpp
@@ -0,0 +1,25 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubStructure(TreeNode* A, TreeNode* B) {
+        if(A == NULL || B == NULL)   return false;
+        if(A->val == B->val && scan(A,B))   return true;  //是否存在满足条件的子结构,按根左右遍历
+        return isSubStructure(A->left,B) || isSubStructure(A->right,B);
+    }
+    bool scan(TreeNode* A, TreeNode* B) {
+        if(B == NULL)  return true;
+        if(A == NULL)  return false;
+        if(A->val != B->val)    return false;  
+        return scan(A->left,B->left) && scan(A->right, B->right);    
+    }
+};
+
