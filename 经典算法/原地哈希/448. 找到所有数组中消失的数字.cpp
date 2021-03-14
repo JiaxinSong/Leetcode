227bb6f990d05ae252cbecee3bd0752a502a3872
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-03-14 17:33:15 -0400

    Update 448. 找到所有数组中消失的数字.cpp

--- 经典算法/原地哈希/448. 找到所有数组中消失的数字.cpp
+++ 经典算法/原地哈希/448. 找到所有数组中消失的数字.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size();
-        for (auto& num : nums) {
-            int x = (num - 1) % n;
-            nums[x] += n;
-        }
-        vector<int> ret;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= n) {
-                ret.push_back(i + 1);
-            }
-        }
-        return ret;
+    for (int i = 0; i < nums.size(); ++i)
+        nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]);
+    vector<int> res;
+    for (int i = 0; i < nums.size(); ++i){
+        if (nums[i] > 0)
+            res.push_back(i+1);
     }
+    return res;
+}
 };
 
