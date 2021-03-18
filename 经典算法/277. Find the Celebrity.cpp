17c7fedae8b692c4c03cc4ae23b6751b2cdc3c60
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-03-18 02:30:45 -0400

    Create 277. Find the Celebrity.cpp

---
+++ 经典算法/277. Find the Celebrity.cpp
@@ -0,0 +1,23 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int result = 0;
+        for (int i = 1; i < n; ++i) {
+            if (knows(result, i)) {
+                result = i;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (result == i) continue;
+            if (knows(result, i) || !knows(i, result)) return -1;
+        }
+        return result;
+    }
+};
+
