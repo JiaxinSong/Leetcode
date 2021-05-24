458789c3451401a0eb02da39e0d61eb0f9cddc10
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-05-24 00:09:45 -0400

    Create 剑指 Offer 05. 替换空格.java

---
+++ 剑指offer_java/剑指 Offer 05. 替换空格.java
@@ -0,0 +1,33 @@
+//把字节数组一部分转成字符串 String(byte[]byte, int offset,int length)
+//同样点击这个构造方法进入，查看解释部分。Offset表示要解码的第一个 byte 的索引，length表示要解码的 byte 数，注意这个数是长度，不是结束的索引位置。
+class Solution {
+    public String replaceSpace(String s) {
+        int length = s.length();
+        char[] array = new char[length * 3];
+        int size = 0;
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                array[size++] = '%';
+                array[size++] = '2';
+                array[size++] = '0';
+            } else {
+                array[size++] = c;
+            }
+        }
+        String newStr = new String(array, 0, size);
+        return newStr;
+    }
+}
+
+class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray())
+        {
+            if(c == ' ') res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+}
