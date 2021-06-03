e28e90074ffa5f700bf642fe4ca62b8bc9e59bc3
JiaxinSong <72943121+JiaxinSong@users.noreply.github.com>
2021-06-03 14:38:27 -0400

    Create 剑指 Offer 41. 数据流中的中位数.java

---
+++ 剑指offer_java/剑指 Offer 41. 数据流中的中位数.java
@@ -0,0 +1,33 @@
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+
+ class MedianFinder {
+    Queue<Integer> A, B;
+    public MedianFinder() {
+        A = new PriorityQueue<>(); // 小顶堆，保存较大的一半
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return (o2-o1);
+            }
+        };
+        B = new PriorityQueue<>(comparator); // 大顶堆，保存较小的一半 (x,y)->(y-x)
+
+    }
+    public void addNum(int num) {
+        if(A.size() != B.size()) {
+            A.add(num);
+            B.add(A.poll());
+        } else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+    public double findMedian() {
+        return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+    }
+}
+
