70c2bc821a5feab2047bc57bca630d370ba0828b
Jiaxin Song <72943121+JiaxinSong@users.noreply.github.com>
2021-06-10 04:04:32 -0400

    Create 剑指 Offer 56 - II. 数组中数字出现的次数 II.java

---
+++ 剑指offer_java/剑指 Offer 56 - II. 数组中数字出现的次数 II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; ++i){
+            int cnt = 0;
+            for(int n : nums){
+                // n & 1 << i 的值大于0即为真
+                if((n & (1 << i))!=0) cnt++;
+            }
+            // 构造只出现一次的那个数字，采用异或的方法生成二进制中的每一位
+            if(cnt % 3 == 1) ans += (1 << i);
+        }
+        return ans;
+    }
+}
