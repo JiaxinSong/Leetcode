e730ed774e5c9ff7e924e61afe08fc0b59a8d96d
Jiaxin Song <72943121+JiaxinSong@users.noreply.github.com>
2021-06-16 18:49:56 -0400

    Create 1071. Greatest Common Divisor of Strings.java

---
+++ java经典/1071. Greatest Common Divisor of Strings.java
@@ -0,0 +1,73 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length(), len2 = str2.length();
+        for (int i = Math.min(len1, len2); i >= 1; --i) { // 从长度大的开始枚举
+            if (len1 % i == 0 && len2 % i == 0) {
+                String X = str1.substring(0, i);
+                if (check(X, str1) && check(X, str2)) {
+                    return X;
+                }
+            }
+        }
+        return "";
+    }
+
+    public boolean check(String t, String s) {
+        int lenx = s.length() / t.length();
+        StringBuffer ans = new StringBuffer();
+        for (int i = 1; i <= lenx; ++i) {
+            ans.append(t);
+        }
+        return ans.toString().equals(s);
+    }
+}
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int len1 = str1.length(), len2 = str2.length();
+        String T = str1.substring(0, gcd(len1, len2));
+        if (check(T, str1) && check(T, str2)) {
+            return T;
+        }
+        return "";
+    }
+
+    public boolean check(String t, String s) {
+        int lenx = s.length() / t.length();
+        StringBuffer ans = new StringBuffer();
+        for (int i = 1; i <= lenx; ++i) {
+            ans.append(t);
+        }
+        return ans.toString().equals(s);
+    }
+
+    public int gcd(int a, int b) {
+        int remainder = a % b;
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            remainder = a % b;
+        }
+        return b;
+    }
+}
+
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!str1.concat(str2).equals(str2.concat(str1))) {
+            return "";
+        }
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    public int gcd(int a, int b) {
+        int remainder = a % b;
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            remainder = a % b;
+        }
+        return b;
+    }
+}
