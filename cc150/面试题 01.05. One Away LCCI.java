bc24c2006a7a24f42eaa683940a98fc1e0748266
Jiaxin Song <72943121+JiaxinSong@users.noreply.github.com>
2021-06-23 03:45:40 -0400

    Create 面试题 01.05. One Away LCCI.java

---
+++ cc150/面试题 01.05. One Away LCCI.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public boolean oneEditAway(String first, String second) {
+
+        int f = first.length();
+        int s = second.length();
+
+        if (Math.abs(f - s) > 1) return false;
+        if (first.equals(second)) return true;
+
+        int count = 0;    // 记录不匹配的次数。
+        int i = 0, j = 0; // 用来指向两字符串的指针
+        while (i < f && j < s) {
+
+            // 两个字符串从前往后开始遍历，如果当前两字符串字符相同就继续比较下一个。
+            if (first.charAt(i++) == second.charAt(j++)) {
+
+                continue;
+            }
+            count++;
+            if (count > 1) return false;
+
+            if (f != s) { // 若不相等，则将短字符串指针前移一位
+
+                if (f > s) j--;
+                else i--;
+            }
+        }
+        return true;
+    }
+}
