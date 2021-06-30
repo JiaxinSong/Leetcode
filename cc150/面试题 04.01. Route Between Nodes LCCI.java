1bb39966aae79d92f316379841460ed0bdc9f76c
Jiaxin Song <72943121+JiaxinSong@users.noreply.github.com>
2021-06-30 13:21:49 -0700

    Update 面试题 04.01. Route Between Nodes LCCI.java

--- cc150/面试题 04.01. Route Between Nodes LCCI.java
+++ cc150/面试题 04.01. Route Between Nodes LCCI.java
@@ -23,3 +23,36 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+class Solution {
+    // 访问状态数组
+    private boolean[] visited = null;
+    public boolean findWhetherExistsPath(int n, int[][] graph, int start, int target) {
+        // 创建访问状态数组
+        this.visited = new boolean[graph.length];
+        // DFS
+        return helper(graph, start, target);
+    }
+
+    private boolean helper(int[][] graph, int start, int target) {
+        // 深度优先搜索
+        for (int i = 0; i < graph.length; ++i) {
+            // 确保当前路径未被访问（该判断主要是为了防止图中自环出现死循环的情况）
+            if (!visited[i]) {
+                // 若当前路径起点与终点相符，则直接返回结果
+                if (graph[i][0] == start && graph[i][1] == target) {
+                    return true;
+                }
+                if (graph[i][1] == target ) {
+                    visited[i]=true;
+                    if(helper(graph,start,graph[i][0])) return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+}
