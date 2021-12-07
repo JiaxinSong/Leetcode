9ef959f38d8c43be59160ab1547ff4aaab60e9d5
Jiaxin Song <72943121+JiaxinSong@users.noreply.github.com>
2021-12-06 18:17:57 -0800

    Update 1044. 最长重复子串.java

--- 分类/Rabin-Karp/1044. 最长重复子串.java
+++ 分类/Rabin-Karp/1044. 最长重复子串.java
@@ -1,54 +1,72 @@
 class Solution {
-    /*
-    Rabin-Karp with polynomial rolling hash.
-        Search a substring of given length
-        that occurs at least 2 times.
-        Return start position if the substring exits and -1 otherwise.
-        */
-    public int search(int L, int a, long modulus, int n, int[] nums) {
-        // compute the hash of string S[:L]
-        long h = 0;
-        for (int i = 0; i < L; ++i) h = (h * a + nums[i]) % modulus;
+    int[] nums;
+    long mod = Integer.MAX_VALUE;
 
-        // already seen hashes of strings of length L
-        HashSet<Long> seen = new HashSet();
-        seen.add(h);
-        // const value to be used often : a**L % modulus
-        long aL = 1;
-        for (int i = 1; i <= L; ++i) aL = (aL * a) % modulus;
+    public String longestDupSubstring(String s) {
+        int length = s.length();
+        nums = new int[length];
+        for (int i = 0; i < length; i++) {
+            nums[i] = s.charAt(i) - 'a';
+        }
 
-        for (int start = 1; start < n - L + 1; ++start) {
-            // compute rolling hash in O(1) time
-            h = (h * a - nums[start - 1] * aL % modulus + modulus) % modulus;
-            h = (h + nums[start + L - 1]) % modulus;
-            if (seen.contains(h)) return start;
-            seen.add(h);
+        //二分确定待查子串长度，若当前子串可以查出重复，则left右移，若不能则right左移
+        int left = 1, right = length;
+        int L;
+        while (left != right) {
+            L = left + (right - left) / 2;
+            if (search(L) != -1) left = L + 1;
+            else right = L;
+        }
+
+        int end = search(left - 1);
+        return end != -1 ? s.substring(end-left+2, end+1) : "";
+
+    }
+
+    public int search(int length) {
+        Map<Long, List<Integer>> seen = new HashMap<>();
+        //first是第一个子串[0, length)的hash值
+        long first = nums[0];
+        //highest = 26^(length-1)，用于后续计算其他子串的hash
+        long highest = 1;
+        for (int i = 1; i < length; i++) {
+            first = (first * 26 + nums[i]) % mod;
+            highest = (highest * 26) % mod;
+        }
+        List<Integer> list = new ArrayList<>();
+        list.add(length-1);
+        seen.put(first, list);
+
+        long temp = first;
+        for (int i = length; i < nums.length; i++) {
+            //最后加一个mod是考虑到前面的值可能是负数
+            temp = (temp*26 - highest*nums[i-length]*26%mod + nums[i] + mod) % mod;
+            List<Integer> isContain = seen.getOrDefault(temp, null);
+            if(isContain == null){
+                List<Integer> tempList = new ArrayList<>();
+                tempList.add(i);
+                seen.put(temp, tempList);
+            }else{
+                for(int index : isContain){
+                    boolean isSame = check(index, i, length);
+                    if(isSame){
+                        return i;
+                    }
+                }
+                isContain.add(i);
+                seen.put(temp, isContain);
+            }
         }
         return -1;
     }
 
-    public String longestDupSubstring(String S) {
-        int n = S.length();
-        // convert string to array of integers
-        // to implement constant time slice
-        int[] nums = new int[n];
-        for (int i = 0; i < n; ++i) nums[i] = (int)S.charAt(i) - (int)'a';
-        // base value for the rolling hash function
-        int a = 26;
-        // modulus value for the rolling hash function to avoid overflow
-        long modulus = (long)Math.pow(2, 32);
-
-        // binary search, L = repeating string length
-        int left = 1, right = n;
-        int L;
-        while (left != right) {
-            L = left + (right - left) / 2;
-            if (search(L, a, modulus, n, nums) != -1) left = L + 1;
-            else right = L;
+    private boolean check(int begin1, int begin2, int length) {
+        for(int i =0; i < length; i++){
+            if(nums[begin1-i] != nums[begin2-i]){
+                return false;
+            }
         }
-
-        int start = search(left - 1, a, modulus, n, nums);
-        return start != -1 ? S.substring(start, start + left - 1) : "";
+        return true;
     }
 }
 
